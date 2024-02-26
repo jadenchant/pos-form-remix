4cc731c670f94742e2656da14bbadfe6614388f3
Jaden Chant <chant1@stolaf.edu>
2024-02-26 12:46:12 -0600

    trying remix

--- README.md
+++ README.md
@@ -1,14 +1,38 @@
-# St. Olaf POS
+# Welcome to Remix!
 
-## Getting Started
+- [Remix Docs](https://remix.run/docs)
 
-```bash
+## Development
+
+From your terminal:
+
+```sh
 npm run dev
-npx run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
 ```
+
+This starts your app in development mode, rebuilding assets on file changes.
+
+## Deployment
+
+First, build your app for production:
+
+```sh
+npm run build
+```
+
+Then run the app in production mode:
+
+```sh
+npm start
+```
+
+Now you'll need to pick a host to deploy it to.
+
+### DIY
+
+If you're familiar with deploying node applications, the built-in Remix app server is production-ready.
+
+Make sure to deploy the output of `remix build`
+
+- `build/`
+- `public/build/`
---
+++ app/root.tsx
@@ -0,0 +1,38 @@
+import { cssBundleHref } from "@remix-run/css-bundle";
+import type { LinksFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import '@shopify/polaris/build/esm/styles.css';
+import enTranslations from '@shopify/polaris/locales/en.json';
+import { AppProvider } from '@shopify/polaris';
+
+export const links: LinksFunction = () => [
+  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+];
+
+export default function App() {
+  return (
+    <AppProvider i18n={enTranslations}>
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+    </AppProvider>
+  );
+}
